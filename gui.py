b0e33a2774fc9e413e937ea855ee86fd778bf14a
hungtooc <hung59349214@gmail.com>
2022-06-05 16:45:39 +0700

    update tach ui & exectuting

--- gui.py
+++ gui.py
@@ -1,42 +1,12 @@
 # -*- coding: utf-8 -*-
 
+# Form implementation generated from reading ui file 'windows.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
 
-import cv2
-import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
-from find_object import *
-
-
-
-class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(np.ndarray)
-    detector, matcher = init_feature('sift')
-    # load object & calculate object feature
-    image_path = "object_01.jpg"
-    object_image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-    object_image = cv2.resize(object_image, (120, 120))
-    object_feature, desc1 = detector.detectAndCompute(object_image, None)  # get feature of object
-    show_corner = False # debug
-    def run(self, camera_index=0):
-        # capture from web cam
-        cap = cv2.VideoCapture(camera_index)
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                homography = find_homography(self.object_image, frame, self.detector, self.matcher, self.desc1, self.object_feature)
-                if homography is not None:
-                    object_coord, object_corners = get_object_coord(
-                        self.object_image, frame, homography)
-                    if object_coord:
-                        cv2.circle(frame, object_coord, 4, (255, 0, 0), -1)
-                        cv2.putText(frame, f"{object_coord}", object_coord,
-                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
-                if self.show_corner == True:
-                    for corner in object_corners:
-                        cv2.circle(frame, corner, 3, (0, 255, 0), -1)
-                self.change_pixmap_signal.emit(frame)
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -62,13 +32,9 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.camera_label.sizePolicy().hasHeightForWidth())
         self.camera_label.setSizePolicy(sizePolicy)
         self.camera_label.setText("")
-        self.camera_label.setPixmap(QtGui.QPixmap("7e5af3311146d8188157.jpg"))
+        self.camera_label.setPixmap(QtGui.QPixmap("../../../../../../home/hungtooc/Pictures/7e5af3311146d8188157.jpg"))
         self.camera_label.setScaledContents(True)
         self.camera_label.setObjectName("camera_label")
-        self.thread = VideoThread()
-        
-        self.thread.change_pixmap_signal.connect(self.update_image)
-        self.thread.start()
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
         self.groupBox.setGeometry(QtCore.QRect(20, 520, 541, 111))
         self.groupBox.setObjectName("groupBox")
@@ -254,25 +220,3 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
         self.pushButton_5.setText(_translate("MainWindow", "PushButton"))
 
-    def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(w, h, Qt.KeepAspectRatio)
-        return QtGui.QPixmap.fromImage(p)
-    
-    def update_image(self, cv_img):
-        """Updates the camera_label with a new opencv image"""
-        qt_img = self.convert_cv_qt(cv_img)
-        self.camera_label.setPixmap(qt_img)
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
