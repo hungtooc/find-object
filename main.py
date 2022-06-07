f068488df87664463af46348bec6fb1867ab73f1
hungtooc <hung59349214@gmail.com>
2022-06-07 12:57:01 +0700

    update detect circle

--- main.py
+++ main.py
@@ -34,43 +34,78 @@ class VideoThread(QThread):
     # object_features = [detector.detectAndCompute(object_image, None) for object_image in object_images]
     
 
-    def run(self, camera_index=2):
-        # capture from web cam
-        cap = cv2.VideoCapture(camera_index)
-        global mark_center, mark_corner, debug
+    def run(self, camera_url="videos/demo-v0.2.mp4"):
+        # camera_url="videos/demo-v0.2.mp4"
+        # camera_url=0
+        # camera_url=1
+        cap = cv2.VideoCapture(camera_url)
+        global mark_center, mark_corner, debug, image_resolution
         while True:
             ret, frame = cap.read()
+            print(frame.shape)
             if ret:
+                circle_max = get_circle(frame)
+                if circle_max is not None:
+                    cv2.rectangle(frame, (circle_max[0], circle_max[1]), (circle_max[0] + circle_max[2], circle_max[1] + circle_max[3]), (255,255,0))
                 # detected_object, object_type, _ = getobject(frame, self.object_images, self.detector, self.matcher, self.object_features)
-                output = getobject(frame, self.object_images, self.detector, self.matcher, self.object_features, debug )
-                # print("type(output)", type(output))
-                if output is not None:
-                    # print("output", output)
-                    bestest_object, confident_score, (object_coord, object_corners) = output[0]
-                    object_type = output[1]
-                    # print("detected_object", object_type)
-                    if bestest_object is not None:
-                        # cv2.imwrite("detected_object.jpg", bestest_object)
-                        # bestest_object = cv2.resize(bestest_object, (240,240))
-                        self.change_currentobject_signal.emit(bestest_object)
-                        self.change_currentobjecttype_signal.emit(object_names[object_type])
+                # output = getobject(frame, self.object_images, self.detector, self.matcher, self.object_features, debug )
+                # # print("type(output)", type(output))
+                # if output is not None:
+                #     # print("output", output)
+                #     bestest_object, confident_score, (object_coord, object_corners) = output[0]
+                #     object_type = output[1]
+                #     # print("detected_object", object_type)
+                #     if bestest_object is not None:
+                #         # cv2.imwrite("detected_object.jpg", bestest_object)
+                #         # bestest_object = cv2.resize(bestest_object, (240,240))
+                #         self.change_currentobject_signal.emit(bestest_object)
+                #         self.change_currentobjecttype_signal.emit(object_names[object_type])
                         
-                    if object_coord:
-                        self.change_currentobjectpos_signal.emit(str(object_coord))
-                        if mark_center:
-                            # print("check box center True")
-                            cv2.circle(frame, object_coord, 4, (255, 0, 0), -1)
-                            # cv2.putText(frame, f"{object_coord}", object_coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
-                        # else:
-                            # print("check box center False")
-                        if mark_corner:
-                            # print("check box corner True")
-                            for corner in object_corners:
-                                cv2.circle(frame, corner, 3, (0, 255, 0), -1)
-                        # else:
-                            # print("check box corner False")
+                #     if object_coord:
+                #         self.change_currentobjectpos_signal.emit(str(object_coord))
+                #         if mark_center:
+                #             # print("check box center True")
+                #             cv2.circle(frame, object_coord, 4, (255, 0, 0), -1)
+                #             # cv2.putText(frame, f"{object_coord}", object_coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+                #         # else:
+                #             # print("check box center False")
+                #         if mark_corner:
+                #             # print("check box corner True")
+                #             for corner in object_corners:
+                #                 cv2.circle(frame, corner, 3, (0, 255, 0), -1)
+                #         # else:
+                #             # print("check box corner False")
                 self.change_camera_signal.emit(frame)
 
+def get_circle(image, min_size = 100):
+    stat_max = None
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.blur(gray,(5,5))
+    adaptive = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,5,2)
+    kernel = np.ones((2,2),np.uint8)
+    erosion = cv2.erode(adaptive,kernel,iterations = 1)
+    kernel = np.ones((5,5),np.uint8)
+    dilation = cv2.dilate(erosion,kernel,iterations = 1)
+    kernel = np.ones((10,10),np.uint8)
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+    
+    stats = cv2.connectedComponentsWithStats(closing)[2]
+    closing = cv2.cvtColor(closing, cv2.COLOR_GRAY2BGR)
+    if len(stats)>1:
+        stats = stats[1:]
+        condition = (0.9 <= stats[:, 2] / stats[:, 3])  *(stats[:, 2] / stats[:, 3] <=1.1) * (stats[:, 2] >= min_size) * (stats[:, 3] >= min_size)
+        stats = stats[condition]
+        if len(stats)>0:
+            stat_max =  stats[np.argmax(stats[:, -1])][:-1]
+            # cv2.rectangle(closing, (stat_max[0], stat_max[1]), (stat_max[0] + stat_max[2], stat_max[1] + stat_max[3]), (255,255,0))
+    
+    # images = (adaptive, erosion, dilation)
+    # stacked_image   = np.hstack(images)
+    # stacked_image = cv2.cvtColor(stacked_image, cv2.COLOR_GRAY2BGR)
+    # image   = np.hstack((image, stacked_image, closing))
+    # cv2.imshow("windows", closing)
+    # cv2.waitKey(1)
+    return stat_max
 
 def convert_cv_qt(cv_img):
     """Convert from an opencv image to QPixmap"""
@@ -126,7 +161,7 @@ if __name__ == "__main__":
     mark_center = ui.checkBox_markcenter.isChecked()
     mark_corner = ui.checkBox_markcorner.isChecked()
     debug = ui.checkBox_markdebug.isChecked()
-
+    image_resolution = (480,640)
     ui.thread = VideoThread()
     
     # update image
